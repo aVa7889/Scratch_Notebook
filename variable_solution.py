8a68854311fd1843586e14621da970b6f90616cd
Ava Lee <avalee@Avas-MacBook-Air.local>
2024-05-25 19:30:16 -0600

    description

--- variable_solution.py
+++ variable_solution.py
@@ -7,4 +7,50 @@ print("Nakia is a professional", profession)
 print("They have been working for", years, "years")
 print("Their hourly wage is ", hourly_wage)
 print("Expert status :", expert_status)
+########################################################################################
+
+user_name = input("Name?")
+print("Name type is", type(user_name))
+
+age = int(input("How old?"))
+print("Age type is", type(age))
+
+true_or_false = bool(input("input?"))
+print("input type is", type(true_or_false))
+
+print("name is " + user_name)
+print("age is " + str(age) + "years old.")
+print("The input " + str(true_or_false))
+
+########################################################################################
+item_cost = float(input("How much does " + item +" cost?"))
+print(
+    "want to buy"
+    + str(quantity)
+    +" "
+    +item_cost
+    + str(cost)
+    + " each."
+)
+#########################################################################################
+print(
+    """ Name:
+    birth:
+    place"""
+)
+print(f"you said: {input}")
+print(f"lowercase: {input.lower()}")
+print(f"Uppercase: {input.upper()}")
+print(f"Title: {input.title()}")
+print(f"Pi to two decimal place is {pi:.2f}\n")
+print(f"i have ${3500:,} to spend")
+dash_string = "-" * 50
+
+#########################################################################################
+
+
+
+
+
+
 
