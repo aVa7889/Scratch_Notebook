d2475f4528b7385e043d333ba6934a210ee20362
aVa7889 <157266652+aVa7889@users.noreply.github.com>
2024-05-25 19:07:25 -0600

    Add files via upload

---
+++ variable_solution.py
@@ -0,0 +1,10 @@
+profession = "software engineer"
+years = 20
+hourly_wage = 72.12
+expert_status = True
+
+print("Nakia is a professional", profession)
+print("They have been working for", years, "years")
+print("Their hourly wage is ", hourly_wage)
+print("Expert status :", expert_status)
+
